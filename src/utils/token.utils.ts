50f41ba6db2b112e667a163df8e1277359967a0c
BaagrieL <raulgabruel12@gmail.com>
2025-07-26 10:29:27 -0300

    :sparkles: feat: Add function to get parsed token from localStorage

--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -1,3 +1,5 @@
+import { RegisterCredentials } from "@/services/authService";
+
 export function theresToken(): boolean {
   return (
     typeof window !== "undefined" && localStorage.getItem("token") !== null
@@ -16,4 +18,29 @@ export function setToken(token: string): void {
   localStorage.setItem("token", token);
 }
 
+function parseToken(token: string) {
+  if (!token) {
+    console.error('Token vazio');
+    return null;
+  }
 
+  try {
+    const payload = token.split('.')[1];
+    const decoded = JSON.parse(atob(payload));
+    return decoded;
+  } catch (e) {
+    console.error('Erro ao decodificar token:', e);
+    return null;
+  }
+}
+
+export function getParsedToken(): RegisterCredentials | null {
+  const parsedToken = parseToken(getToken() || '');
+
+  if (!parsedToken) {
+    console.error('Token nulo');
+    return null;
+  }
+
+  return parsedToken;
+}
